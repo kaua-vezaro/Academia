2c0d3b2840a94e09cd0f61ab88c60fa75cce90b6
Kauã Moraes Vezaro <kauavezaro2006@gmail.com>
2026-06-13 10:03:38 -0300

    Comentado

--- logout.php
+++ logout.php
@@ -1,6 +1,10 @@
+<!-- Esse código entra em ação quando eu aperto em sair, dai eu sou redirecionado para à página de login, saindo do usuário no qual eu estava logado -->
+
 <?php
 session_start();
+// Encerra completamente a sessão atual e remove todas as variáveis armazenas em $_SESSION
 session_destroy();
+// Redireciona o usuário de volta para a tela de login
 header("Location: login.php");
 exit;
 ?>  
